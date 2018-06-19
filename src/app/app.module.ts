35b050c85d22ab085ed136be7cc7a8099a28bba1
alexsanqp <kryhtenko@mail.ru>
2018-06-19 18:31:31 +0300

    Init and added ssr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_ID, Inject, NgModule, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports     : [
+        BrowserModule.withServerTransition({
+            appId: 'enkonix',
+        }),
+        AppRoutingModule,
+    ],
+    providers   : [],
+    bootstrap   : [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+    constructor(
+        @Inject(PLATFORM_ID) private platformId: Object,
+        @Inject(APP_ID) private appId: string,
+    ) {
+        const platform = isPlatformBrowser(platformId)
+            ? 'in the browser'
+            : 'on the server';
+
+        console.log(`Running ${platform} with appId=${appId}`);
+    }
+}
