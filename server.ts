1371f6c99d4f15287629c4bf75e26425e6f5809f
alexsanqp <kryhtenko@mail.ru>
2018-06-23 21:42:40 +0300

    Added ssl

--- server.ts
+++ server.ts
@@ -2,7 +2,8 @@ import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
 
 import { enableProdMode } from '@angular/core';
-
+import * as fs from 'fs';
+import * as https from 'https';
 import * as express from 'express';
 import { join } from 'path';
 
@@ -11,13 +12,24 @@ import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
 enableProdMode();
 
-const app = express();
-
+const app         = express();
 const PORT        = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist');
 
-// * NOTE :: leave this as require() since this file is built Dynamically from webpack
 const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main');
+
+const credentials = {
+    key : null,
+    cert: null,
+};
+
+if (process.env.NODE_ENV === 'production') {
+    credentials.key  = fs.readFileSync('/etc/letsencrypt/live/pro-beauty.com.ua/privkey.pem',
+        'utf8');
+    credentials.cert = fs.readFileSync('/etc/letsencrypt/live/pro-beauty.com.ua/cert.pem', 'utf8');
+}
+
+const httpsServer = https.createServer(credentials, app);
 
 app.engine('html', ngExpressEngine({
     bootstrap: AppServerModuleNgFactory,
@@ -29,20 +41,14 @@ app.engine('html', ngExpressEngine({
 app.set('view engine', 'html');
 app.set('views', join(DIST_FOLDER, 'browser'));
 
-// TODO: implement data requests securely
 app.get('/api/*', (req, res) => {
     res.status(404).send('data requests are not supported');
 });
-
-// Server static files from /browser
 app.get('*.*', express.static(join(DIST_FOLDER, 'browser')));
-
-// All regular routes use the Universal engine
 app.get('*', (req, res) => {
     res.render('index', {req});
 });
 
-// Start up the Node server
-app.listen(PORT, () => {
+httpsServer.listen(PORT, () => {
     console.log(`Node server listening on http://localhost:${PORT}`);
 });
